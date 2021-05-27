a7ef7c8b2f9c0e084925e09055830e19ef9cfca9
utopier <utopier2025@gmail.com>
2021-05-27 17:08:40 +0900

    Beta 1 Update

--- src/ormconfig.ts
+++ src/ormconfig.ts
@@ -1,10 +1,10 @@
 module.exports =  {
     type: 'mysql',
     database: 'blogdb',
-    host: process.env.AWS_RDS_ENDPOINT,
+    host: process.env.DB_ENDPOINT,
     port: 3306,
-    username: process.env.AWS_RDS_USERNAME,
-    password: process.env.AWS_RDS_PASSWORD,
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
     entities: ['entities/**/*.*'],
     migrations: ['src/migration/**/*.ts'],
     subscribers: ['src/subscriber/**/*.ts'],
